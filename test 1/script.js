e5dfe317d97b887d0edc2055f529072e4e3d244b
vanity <97385744+vanityway@users.noreply.github.com>
2022-01-23 10:31:17 +0300

    Add files via upload

---
+++ test 1/script.js
@@ -0,0 +1,61 @@
+// // var firstNumber = prompt()
+// // var secondNumber = prompt() 
+// // alert(+firstNumber + Number(secondNumber))
+// // console.log(typeof(firstNumber))
+ 
+
+//  var el = document.getElementById ("js")
+// // console.log(el)
+// // el.innerHTML = "TEXT"
+//  var ol = document.getElementById ("btn")
+// // console.log(ol)
+
+
+// // btn.onclick = function () {
+// //     el.innerHTML = "codding"
+// //     el.style.color = "#FF4543"
+// // }
+
+// var el2 = document.getElementById ("newbtn")
+// el2.onclick = function () {
+//     el2.innerHTML = "life"
+//     el2.style.backgroundImage = "linear-gradient(90deg, #020024 0%, #090979 35%, #00d4ff 100%)"
+
+
+// }
+
+// if 
+// else if
+//  else
+//  switch - case
+
+   //  if (age == 0) {
+    //      alert(" Интернет в 0 лет")
+    //  }
+    //  else if (age < 18)
+    //  alert("Тебе сюда нельзя")
+    //  else {
+    //      alert("Вам разрешен вход")
+    //  }
+        
+    // switch (age) {
+    //     case 0: alert("Интернет в 0 лет")
+    //          break;
+    //     case 18: alert("Тебе сюда нельзя!") 
+    //          break;
+    //     default: alert("Привет незнакомец")
+    // }
+
+        var age = +prompt("Сколько вам лет?")
+
+        age < 18 ? alert("Тебе сюда нельзя") : 
+        age == 20 ? alert("Тебе 20") : alert("Привет незнакомец")
+
+
+    
+
+
+
+         
+
+
